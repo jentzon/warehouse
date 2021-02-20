7502bc8a01c354e8aa5d973d19cf3ba4ca21b4c3
Johan Lorentzon <johan.lorentzon@layer10.se>
2021-02-20 10:27:23 +0100

    Updated with CORS config

--- warehouse-server/src/server.ts
+++ warehouse-server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { applyApiPaths } from './apiPaths';
+import cors from 'cors';
 
 // TODO: Apply prettier config
 
@@ -8,6 +9,7 @@ const port = 3050;
 
 // Server setup
 const server = express();
+server.use(cors());
 server.use(express.json());
 
 // Path setup and DB
